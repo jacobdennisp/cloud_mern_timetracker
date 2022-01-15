6218dd93ac044806922cd555fb7b78a19986ab9a
Coder-1002 <79767012+Coder-1002@users.noreply.github.com>
2022-01-15 22:26:38 +0530

    Delete keys.js

--- backend/config/keys.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-//     mongoURI: "mongodb+srv://admin:Jake@12345@cluster0-8lbwj.mongodb.net/timer?retryWrites=true&w=majority",
-    mongoURI: "mongodb+srv://touseef_db_admin:<Touseef@$1002>@merncluster.zz56z.mongodb.net/myFirstDatabase?retryWrites=true&w=majority,
-    secretOrKey: "secret"
-  };
